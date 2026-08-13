9647e2b73780dcec6e1244f382e527bbc7ca0b37
harimyadayala <dayalaharimya@gmail.com>
2026-08-13 14:50:25 +0530

    Update factorial

--- factorial.java
+++ factorial.java
@@ -1,8 +1,8 @@
 public class factorial {
-    /*public static void main(String[] args) {
+    public static void main(String[] args) {
         int number = 5; // Example number to calculate factorial
         System.out.println("Factorial of " + number + " is: " + result);
-    }*/
+    }
 
     public static long compute(int n) {
         if (n < 0) {
